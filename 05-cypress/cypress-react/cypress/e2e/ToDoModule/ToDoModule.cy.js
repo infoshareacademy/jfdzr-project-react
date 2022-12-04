b4c35e369e50f0f9312cd0e0adbff6ce37814145
Maciej Walentynowicz <maciej.walentynowicz@gmail.com>
2022-12-04 17:11:05 +0100

    2nd challenge done

--- 05-cypress/cypress-react/cypress/e2e/ToDoModule/ToDoModule.cy.js
+++ 05-cypress/cypress-react/cypress/e2e/ToDoModule/ToDoModule.cy.js
@@ -3,7 +3,7 @@ describe("Todo Module Tests", () => {
     cy.visit("http://localhost:3000");
   });
   describe("Adding todo tests", () => {
-    it("should assert if add button id disabled by default", () => {
+    it.skip("should assert if add button id disabled by default", () => {
       cy.get("#addNewToDo").should("be.visible");
       cy.get("#input").should("be.visible");
       cy.get("#addNewToDo").should("be.disabled");
@@ -12,6 +12,28 @@ describe("Todo Module Tests", () => {
       cy.contains("zadanie1").should("be.visible");
       cy.get("#xButton").should("be.visible");
       cy.get("#editButton").should("be.visible");
+    });
+    it.only("should change style on click task", () => {
+      cy.get("#addNewToDo").should("be.visible");
+      cy.get("#input").should("be.visible");
+      cy.get("#addNewToDo").should("be.disabled");
+      cy.get("#input").type("zadanie1");
+      cy.get("#addNewToDo").click();
+      cy.contains("zadanie1").should("be.visible");
+      cy.get("#xButton").should("be.visible");
+      cy.get("#editButton").should("be.visible");
+      // break
+      cy.get("#editDescription").should(
+        "have.css",
+        "text-decoration",
+        "rgb(0, 0, 0)"
+      );
+      cy.get("#editDescription").first().click();
+      cy.get("#editDescription").should(
+        "have.css",
+        "text-decoration",
+        "line-through rgb(0, 0, 0)"
+      );
     });
   });
 });
