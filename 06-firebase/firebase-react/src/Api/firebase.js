6544387f6c568f2a07accec9426bd6c56b64bb35
Maciej Walentynowicz <maciej.walentynowicz@gmail.com>
2022-12-04 20:51:12 +0100

    made procejt with firebase
    Co-authored-by: Olga Grochowska <olga-grochowska@users.noreply.github.com>
    Co-authored-by: WIKTOR SOBCZAK <WiktorSobczak@users.noreply.github.com>

---
+++ 06-firebase/firebase-react/src/Api/firebase.js
@@ -0,0 +1,8 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { firebaseConfig } from "./firebase.config";
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+
+export default db;
--- 06-firebase/firebase-react/src/App.js
+++ 06-firebase/firebase-react/src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import db from "./Api/firebase";
+import { addDoc, collection, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [actors, setActors] = useState([]);
+  const actorsCollection = collection(db, "Actors");
+
+  const getActors = () => {
+    getDocs(actorsCollection).then((querySnapshot) => {
+      // console.log(querySnapshot);
+      const actors = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      // console.log(actors);
+      setActors(actors);
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDafault();
+
+    const { firstName, lastName, age } = e.target;
+
+    const actor = {
+      firstName: firstName.value,
+      lastName: lastName.value,
+      age: age.value,
+    };
+
+    addDoc(actorsCollection, actor).then(() => {
+      getActors().then((actors) => setActors(actors));
+      e.target.reset();
+    });
+  };
+  useEffect(() => {
+    getActors();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="firstName">Podaj imię:</label>
+          <input type="text" name="firstName" id="firstName" />
+        </div>
+        <div>
+          <label htmlFor="lastName">Podaj nazwisko:</label>
+          <input type="text" name="lastName" id="lastName" />
+        </div>
+        <div>
+          <label htmlFor="age">Podaj wiek:</label>
+          <input type="number" name="age" id="age" />
+        </div>
+        <button>Dodaj aktora</button>
+      </form>
+      <ul>
+        {actors.map(({ id, firstName, lastName, age }) => (
+          <li key={id}>
+            <p>Imię: {firstName}</p>
+            <p>Nazwisko: {lastName}</p>
+            <p>Wiek: {age}</p>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
