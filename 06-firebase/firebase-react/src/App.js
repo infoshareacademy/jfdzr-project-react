5142a1638e2d2d7089c47f86f3816ade56e96b44
Katarzyna Skibowska <kat.skibowska@gmail.com>
2022-12-04 20:51:08 +0100

    Solutions to challenges 1 and 2 added
    Co-authored-by: Witold Nawrot <witoldnaw@users.noreply.github.com>
    Co-authored-by: Jakub Bedynek <JackRaven611@users.noreply.github.com>

--- 06-firebase/firebase-react/src/App.js
+++ 06-firebase/firebase-react/src/App.js
@@ -1,25 +1,83 @@
 import logo from './logo.svg';
 import './App.css';
+import { collection, getDocs } from 'firebase/firestore'
+import db from './Api/firebase';
+import {useEffect , useState} from "react";
+import { addDoc } from 'firebase/firestore'
+import { doc, setDoc } from 'firebase/firestore'
+
+const ActorsList = ({ actors }) => {
+  return actors.map((actor) => (
+     (
+      
+      <ul>
+        <li>{actor.firstName}</li>
+        <li>{actor.lastName}</li>
+        <li>{actor.age}</li>
+      </ul>
+      
+      ))
+  )
+}
+
 
 function App() {
+
+  const [actors, setActors] = useState([])
+
+  const getActors = () => {
+    const actorsCollection = collection(db, 'Actors')
+    getDocs(actorsCollection).then(querySnapshot => {
+      const actors = querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data(),
+      }))
+      
+      setActors(actors)
+    })
+  }
+    
+  console.log(actors)
+
+  useEffect(() => {
+    getActors()
+  }, []  )
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        <ActorsList actors={actors}/>
+        <ActorsForm />
+      
     </div>
   );
 }
 
+
+const ActorsForm = () => {
+
+  function handleSubmit (e) {
+    e.preventDefault()
+    // e.target.firstName
+    console.log(e.target.firstName.value)
+  }
+
+  return (
+    <>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="firstName">First Name:</label>
+      <input id='firstName'/>
+      <label htmlFor="lastName">Last Name:</label>
+      <input id='lastName'/>
+      <label htmlFor='age'>Age: </label>
+      <input id='age'/>
+    <button>Add Actor</button>
+    </form>
+    </>
+  )
+}
+
 export default App;
+
+
